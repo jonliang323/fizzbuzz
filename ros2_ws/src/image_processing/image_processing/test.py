0c4b5b945e3dc90bd3b78ef99c3c75ce028c21e6
xcode003 <xcode003@gmail.com>
2025-01-19 00:02:20 -0500

    Fixed .txt mistitle

--- ros2_ws/src/image_processing/image_processing/test.py
+++ ros2_ws/src/image_processing/image_processing/test.py
@@ -13,7 +13,7 @@ try:
         old_file_path = os.path.join(folder_path, file_name)
 
         # Create the new file name
-        new_file_name = f"{i}.txt"
+        new_file_name = f"{i}.png"
         new_file_path = os.path.join(folder_path, new_file_name)
 
         # Rename the file
