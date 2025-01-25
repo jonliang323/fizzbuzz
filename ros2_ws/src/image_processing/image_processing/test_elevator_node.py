cef85a04f321bdbed8a79a9a5e0ef33d85df19da
Team 15 <maslab-2025-team-15@mit.edu>
2025-01-25 13:51:20 -0500

    elevator working in elevator_test_node

--- ros2_ws/src/image_processing/image_processing/test_elevator_node.py
+++ ros2_ws/src/image_processing/image_processing/test_elevator_node.py
@@ -10,96 +10,137 @@ class ElevatorTestNode(Node): #testing elevator and duck
         super().__init__('test_elevator_subscriber')
         self.clock = Clock()
         self.prev_time = self.clock.now()
+        motor_msg = MotorCommand()
         self.i = 0
-        self.elevator_state = 'idle'
+        self.elevator_state = 'open claws'
         self.elevator_timer_count = 0
         self.duck_timer_count = 0
-        self.angle1_elev = 0
-        self.angle2_claw = 0
+        self.angle1_elev = 90
+        self.angle2_claw = -40
         self.angle3_flap = 0
-        self.angle4_duck = 0
+        self.elevator = False
+        # self.angle4_duck = 0
         self.elevator_control = self.create_timer(0.5, self.elevator_callback)
         self.motor_pub = self.create_publisher(MotorCommand, "motor_command", 10)
 
     
     def elevator_callback(self):
+        # print("callback triggered")
         motor_msg = MotorCommand()
-        dc = motor_msg.drive_motors
-        servo = motor_msg.actuate_motors
-        servo.angle1_elev = self.angle1_elev
-        servo.angle2_claw = self.angle2_claw
-        servo.angle3_flap = self.angle3_flap
-        servo.angle4_duck = self.angle4_duck
-
-        self.activate_elevator()
-
-        self.motor_pub.publish(motor_msg)
-
-    def activate_elevator(self):
-        motor_msg = MotorCommand()
-
-        #elevator starts: angle1 = 90 (elevator up) angle2 = -25 (claws closed) angle3 = 0 (flap open)
-        self.elevator_state == 'open claws'
-        self.elevator_timer_count = 0
+        # dc = motor_msg.drive_motors
+        # servo = motor_msg.actuate_motors
+        motor_msg.actuate_motors.angle1_elev = self.angle1_elev
+        motor_msg.actuate_motors.angle2_claw = self.angle2_claw
+        motor_msg.actuate_motors.angle3_flap = self.angle3_flap
+        # motor_msg.actuate_motors.angle4_duck = self.angle4_duck
         
 
-        if self.elevator_state == 'open claws':
-            self.angle2_claw = -40  #claws open
-            self.elevator_timer_count += 1
+        self.activate_elevator()
+        self.motor_pub.publish(motor_msg)
 
-            if self.elevator_timer_count >= 4:  #wait 2 seconds
-                self.elevator_state = 'elev move down'
-                self.elevator_timer_count = 0
 
-        elif self.elevator_state == 'elev move down':
-            self.angle1_elev = -90  #elevator moves down
-            self.angle3_flap = 90   #flap closes
+        
 
-            self.elevator_timer_count += 1
-            if self.elevator_timer_count >= 4:  
-                self.elevator_state = 'close claws'
-                self.elevator_timer_count = 0
+    def activate_elevator(self):
+        if self.elevator == False:
+            # motor_msg = MotorCommand()
 
-        #return elevator to starting position
-        elif self.elevator_state == 'close claws':
-            self.angle2_claw = -25  #claws close
-            self.elevator_timer_count += 1
-            if self.elevator_timer_count >= 4:  
-                self.elevator_state = 'elev move up'
-                self.elevator_timer_count = 0
+            # motor_msg.actuate_motors.angle1_elev = self.angle1_elev
+            # motor_msg.actuate_motors.angle2_claw = self.angle2_claw
+            # motor_msg.actuate_motors.angle3_flap = self.angle3_flap
 
-        elif self.elevator_state == 'elev move up':
-            self.angle1_elev = -90  #elevator moves up while holding block
-            self.angle3_flap = 90   #flap opens, ready for next block
-            self.elevator_timer_count += 1
+            #elevator starts: angle1 = 90 (elevator up) angle2 = -40 (claws closed) angle3 = 0 (flap closed)
+            # self.angle1_elev = 90
+            # self.angle2_claw = -40
+            # self.angle3_flap = -90
+            # print(f'state: {self.elevator_state}')
+            # print(f"State: {self.elevator_state}, Timer Count: {self.elevator_timer_count}")
 
-            if self.elevator_timer_count >= 4:  
-                self.elevator_state = 'idle'
-                self.elevator_timer_count = 0
+            # if (self.clock.now() - self.prev_time).nanoseconds/1e9 > 2:
+            #     self.i = self.i + 1
+            #     self.prev_time = self.clock.now()
+
+        
+
+            if self.elevator_state == 'open claws':
+            # if self.i == 0:
+                self.angle1_elev = 90
+                self.angle3_flap = -90 #flap open...if we want default flap to be closed, we can open it above when we set block_intake to be true
+                self.angle2_claw = -45  #claws open
+                #robot should move forward at this point. 
+
+                self.elevator_timer_count += 1
+                print(f'state: open claws ------angles: elev: {self.angle1_elev} claw: {self.angle2_claw} flap: {self.angle3_flap}')
+
+                if self.elevator_timer_count >= 4:  #wait 2 seconds
+                    self.elevator_state = 'elev move down'
+                    print(f'state going to: {self.elevator_state}')
+                    self.elevator_timer_count = 0
+
+            elif self.elevator_state == 'elev move down':
+            # elif self.i == 1:
+                self.angle1_elev = -90  #elevator moves down
+                self.angle3_flap = 0    #flap closes 
+                self.angle2_claw = -45
+                print(f'state: elev moving down------angles: elev: {self.angle1_elev} claw: {self.angle2_claw} flap: {self.angle3_flap}')
+
+                self.elevator_timer_count += 1
+                if self.elevator_timer_count >= 4:  
+                    self.elevator_state = 'close claws'
+                    print(f'state going to: {self.elevator_state}')
+                    self.elevator_timer_count = 0
+
+            #return elevator to starting position
+            elif self.elevator_state == 'close claws':
+            # elif self.i == 2:
+                self.angle1_elev = -90
+                self.angle3_flap = 0 
+                self.angle2_claw = -20  #claws close
+                self.elevator_timer_count += 1
+                print(f'state: claws close------angles: elev: {self.angle1_elev} claw: {self.angle2_claw} flap: {self.angle3_flap}')
+
+                if self.elevator_timer_count >= 4:  
+                    self.elevator_state = 'elev move up'
+                    print(f'state going to: {self.elevator_state}')
+                    self.elevator_timer_count = 0
+
+            elif self.elevator_state == 'elev move up':
+            # elif self.i == 3:
+                self.angle1_elev = 90  #elevator moves up while holding block
+                self.angle3_flap = -90   #flap opens
+                self.angle2_claw = -20
+                self.elevator_timer_count += 1
+                print(f'state: elev moving up------angles: elev: {self.angle1_elev} claw: {self.angle2_claw} flap: {self.angle3_flap}')
+
+                if self.elevator_timer_count >= 4:  
+                    self.elevator_state = 'idle'
+                    print(f'state going to: {self.elevator_state}')
+                    self.elevator_timer_count = 0
+            
+            if self.elevator_state == 'idle':
+            # elif self.i == 4:
+                self.angle1_elev = 90  
+                self.angle3_flap = 0   
+                self.angle2_claw = -20
+                print(f'state: idle ------angles: elev: {self.angle1_elev} claw: {self.angle2_claw} flap: {self.angle3_flap}')
+                # print(f'start angles: elev: {self.angle1_elev}, claw: {self.angle2_claw}, flap: {self.angle3_flap}')
+
+                if self.elevator_timer_count >= 4:  #wait 2 seconds
+                    self.elevator_state = 'open claws'
+                    print(f'state going to: {self.elevator_state}')
+                    self.elevator_timer_count = 0
+                self.elevator = True
+
+
 
 
 
 def main(args=None):
     rclpy.init()
     node = ElevatorTestNode()
-
-    try:
-        while rclpy.ok():
-            rclpy.spin_once(node, timeout_sec=0.1)
-
-            # Check for keyboard input
-            if keyboard.is_pressed('e'):
-                node.get_logger().info("Elevator activated!")
-                node.activate_elevator()
-
-            elif keyboard.is_pressed('q'):
-                node.get_logger().info("Exiting Elevator Tester.")
-                break
-    except KeyboardInterrupt:
-        pass
-    finally:
-        node.destroy_node()
-        rclpy.shutdown()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == "__main__":
     main()
--- ros2_ws/src/image_processing/launch/image_processing_launch.py
+++ ros2_ws/src/image_processing/launch/image_processing_launch.py
@@ -1,24 +1,60 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+# def generate_launch_description():
+#     # cube_detect_node = Node(
+#     #     package='image_processing',
+#     #     executable='cube_detect_subscriber',
+#     #     output='screen'
+#     # )
+
+#     # test_motor_node = Node(
+#     #     package='image_processing',
+#     #     executable='test_motor_subscriber',
+#     #     output='screen'
+#     # )
+
+#     # state_machine_node = Node(
+#     #     package = 'image_processing',
+#     #     executable = 'state_machine',
+#     #     output = 'screen'
+#     # )
+
+#     raven_node = Node(
+#         package='image_processing',
+#         executable='raven_subscriber',
+#         output='screen'
+#     )
+
+#     test_elevator_node = Node(
+#         package='image_processing',
+#         executable='test_elevator_subscriber',
+#         output='screen'
+#     )
+
+#     return LaunchDescription([test_elevator_node, raven_node])
+#     # return LaunchDescription([cube_detect_node, state_machine_node, raven_node])
+
+# def generate_launch_description():
+    
+
+#     test_motor_node = Node(
+#         package='image_processing',
+#         executable='test_motor_subscriber',
+#         output='screen'
+#     )
+
+#     raven_node = Node(
+#         package='image_processing',
+#         executable='raven_subscriber',
+#         output='screen'
+#     )
+
+
+#     return LaunchDescription([test_motor_node, raven_node])
+
 def generate_launch_description():
-    # cube_detect_node = Node(
-    #     package='image_processing',
-    #     executable='cube_detect_subscriber',
-    #     output='screen'
-    # )
-
-    # test_motor_node = Node(
-    #     package='image_processing',
-    #     executable='test_motor_subscriber',
-    #     output='screen'
-    # )
-
-    # state_machine_node = Node(
-    #     package = 'image_processing',
-    #     executable = 'state_machine',
-    #     output = 'screen'
-    # )
+    
 
     raven_node = Node(
         package='image_processing',
@@ -33,4 +69,4 @@ def generate_launch_description():
     )
 
     return LaunchDescription([test_elevator_node, raven_node])
-    # return LaunchDescription([cube_detect_node, state_machine_node, raven_node])
+    
